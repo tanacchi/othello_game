6c0ed4c68945fc3f3889b721a248690925fe33e6
tanacchi <q111026d@mail.kyutech.jp>
2017-03-29 02:32:49 +0900

    Add reverse stone counter
    裏返せる石の数を数える関数導入（動かない）

--- othello_draft.cpp
+++ othello_draft.cpp
@@ -6,7 +6,7 @@
 
 #define BOARD_SIZE 8
 
-enum class STONE {
+enum class Stone {
   WHITE,
   BLACK,
   SPACE,
@@ -14,33 +14,36 @@ enum class STONE {
 };
 
 class BoardMaster {
-  STONE board[BOARD_SIZE][BOARD_SIZE];
-  STONE active_stone;
+  Stone board[BOARD_SIZE][BOARD_SIZE];
+  Stone active_stone;
 public:
   BoardMaster();
   void init_board();
   void show_board();
-  const char convert_num_to_char(STONE stone);
+  const char convert_num_to_char(Stone stone);
   bool is_stone_space(int x, int y);
   bool is_inside_board(int x, int y);
   bool is_valid_hand(int x, int y);
   void insert_stone(int x, int y); 
-  void set_active_stone(STONE stone);
+  void set_active_stone(Stone stone);
   bool can_continue();
+  int count_reverse_stone(int x, int y);
+  int get_reverse_length(int* direction);
+  Stone get_enemy_stone();
 };
 
 BoardMaster::BoardMaster() {
   init_board();
-  active_stone = STONE::WHITE;
+  active_stone = Stone::WHITE;
   show_board();
 }
 
 void BoardMaster::init_board() {
   for (int i = 0; i < BOARD_SIZE; i++)
     for (int j = 0; j < BOARD_SIZE; j++)
-      board[i][j] = STONE::SPACE;
-  board[3][3] = board[4][4] = STONE::WHITE;
-  board[3][4] = board[4][3] = STONE::BLACK;
+      board[i][j] = Stone::SPACE;
+  board[3][3] = board[4][4] = Stone::WHITE;
+  board[3][4] = board[4][3] = Stone::BLACK;
 }
 
 void BoardMaster::show_board() {
@@ -56,25 +59,25 @@ void BoardMaster::show_board() {
   std::cout << "---------------------------" << std::endl;
 }
 
-const char BoardMaster::convert_num_to_char(STONE src) {
+const char BoardMaster::convert_num_to_char(Stone src) {
   switch (src) {
-  case STONE::SPACE: return ' ';
-  case STONE::BLACK: return 'O';
-  case STONE::WHITE: return 'X';
-  case STONE::DOT:   return '*';
+  case Stone::SPACE: return ' ';
+  case Stone::BLACK: return 'O';
+  case Stone::WHITE: return 'X';
+  case Stone::DOT:   return '*';
   }
 }
 
-void BoardMaster::set_active_stone(STONE stone) {
+void BoardMaster::set_active_stone(Stone stone) {
   active_stone = stone;
 }
 
-bool BoardMaster::is_inside_board(int x, int y) {
+inline bool BoardMaster::is_inside_board(int x, int y) {
   return (0 <= x && x < BOARD_SIZE) && (0 <= y && y < BOARD_SIZE);
 }
 
 bool BoardMaster::is_stone_space(int x, int y) {
-  return (board[y][x] == STONE::SPACE);
+  return (board[y][x] == Stone::SPACE);
 }
 
 bool BoardMaster::is_valid_hand(int x, int y) {
@@ -86,24 +89,50 @@ void BoardMaster::insert_stone(int x, int y) {
 }
 
 bool BoardMaster::can_continue() {
+  int count_space, count_black, count_white = 0;
   for (int i = 0; i < BOARD_SIZE; i++)
-    for (int j = 0; j < BOARD_SIZE; j++)
-      if (board[i][j] == STONE::SPACE) return true;
-  return false;
+    for (int j = 0; j < BOARD_SIZE; j++) 
+      switch (board[i][j]) {
+      case Stone::SPACE: count_space++; break;
+      case Stone::BLACK: count_black++; break;
+      case Stone::WHITE: count_white++; break;
+      }
+  return (count_space && count_black && count_white);
+}
+
+int BoardMaster::count_reverse_stone(int x, int y) {
+  Stone enemy_stone = get_enemy_stone();
+  int reverse_stone_count;
+  int dx[8] = { 0, 1, 1, 1, 0,-1,-1,-1 };
+  int dy[8] = { 1, 1, 0,-1,-1,-1, 0, 1 };
+  
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; is_inside_board(x + j*dx[i], y + j*dy[i]); j++) {
+      Stone target = board[y + j*dy[i]][x + j*dx[i]];
+      if (target == enemy_stone) continue; 
+      else if (target == active_stone) { reverse_stone_count += j; break; }
+      else break;
+    }
+  }
+  return reverse_stone_count;
+}
+
+Stone BoardMaster::get_enemy_stone() {
+  return (active_stone == Stone::WHITE) ? Stone::BLACK : Stone::WHITE;
 }
 
 class Player {
-  STONE MY_STONE;
+  Stone MY_Stone;
   int hand_x, hand_y;
 public:
-  void set_my_stone(STONE stone);
+  void set_my_stone(Stone stone);
   void set_hand(const int input_x, const int input_y);
-  STONE get_my_stone();
+  Stone get_my_stone();
   void get_hand(int &x, int &y);
 };
 
-void Player::set_my_stone(STONE stone) {
-  MY_STONE = stone;
+void Player::set_my_stone(Stone stone) {
+  MY_Stone = stone;
 }
 
 void Player::set_hand(const int input_x, const int input_y) {
@@ -111,8 +140,8 @@ void Player::set_hand(const int input_x, const int input_y) {
   hand_y = input_y;
 }
 
-STONE Player::get_my_stone() {
-  return MY_STONE;
+Stone Player::get_my_stone() {
+  return MY_Stone;
 }
 
 void Player::get_hand(int &x, int &y) {
@@ -150,25 +179,26 @@ int main() {
 
   BoardMaster board;
   ComputerPlayer cpu[2];
-  Player *active_player;
 
-  cpu[0].set_my_stone(STONE::WHITE);
-  cpu[1].set_my_stone(STONE::BLACK);
+  cpu[0].set_my_stone(Stone::WHITE);
+  cpu[1].set_my_stone(Stone::BLACK);
     
   int turn;
   while (board.can_continue()) {
-    active_player = &cpu[turn % 2];
-    board.set_active_stone(active_player->get_my_stone());
+    board.set_active_stone(cpu[turn % 2].get_my_stone());
+
     int x, y;
     do {
       cpu[turn % 2].set_hand_random();
-      active_player->get_hand(x, y);
-    } while (!board.is_valid_hand(x, y));    
+      cpu[turn % 2].get_hand(x, y);
+    } while (!board.is_valid_hand(x, y));
+    
+    int reverse_stone = board.count_reverse_stone(x, y);
+    std::cout << "reverse_stone = " << reverse_stone << std::endl;
     board.insert_stone(x, y);
     board.show_board();
     turn++;
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-  }
-  
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  }   
   return 0;
 }
