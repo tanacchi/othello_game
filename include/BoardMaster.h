d12b34cbb23d087f9672bd6c038ef416b3310821
tanacchi <q111026d@mail.kyutech.jp>
2017-04-03 13:17:23 +0900

    Add include directory

---
+++ include/BoardMaster.h
@@ -0,0 +1,13 @@
+#include "othello_base.h"
+#include "BoardBase.h"
+
+class BoardMaster : public BoardBase {
+public:
+  bool can_continue();
+  bool is_available_position(int x, int y);
+  int count_reversible_stone(int x, int y);
+  int count_stone(Stone target);
+  void put_dot_stone();
+  void remove_dot_stone();
+  void reverse_stone(int x, int y);
+};
---
+++ include/othello_base.h
@@ -0,0 +1,20 @@
+#ifndef _HEADER_H_
+#define _HEADER_H_
+
+#include <iostream>
+#include <chrono>
+#include <iostream>
+#include <list>
+#include <random>
+#include <thread>
+
+#define BOARD_SIZE 8
+
+enum class Stone {
+  WHITE,
+  BLACK,
+  SPACE,
+  DOT
+};
+
+#endif _HEADER_H_
