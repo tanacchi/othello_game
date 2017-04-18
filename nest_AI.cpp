73943b33ccdfc7c79b78e7854e8d6ee57d8e865a
tanacchi <q111026d@mail.kyutech.jp>
2017-04-18 16:54:18 +0900

    Refact nestAI tester

--- nest_AI.cpp
+++ nest_AI.cpp
@@ -10,29 +10,26 @@ class NestClass {
 public:
   NestClass(int dept) {
     num = 1;
-    serial_num = global;
-    std::cout << "Hello ! " << serial_num << ' ';
-    std::cout << global++ << std::endl;
-    for (int i = 0; i < dept-1; i++)
-      sub.push_back(NestClass(dept-1));
+    serial_num = global++;
+    std::cout << "Hello ! " << serial_num << std::endl;
+    for (int i = 0; i < dept-1; i++) sub.push_back(NestClass(dept-1));
   }
   int sum() {
     if (!sub.empty()) {
       for (int i = 0; i < sub.size(); i++) num += sub[i].sum();
-      std::cout << "Hi" << serial_num << std::endl;  
+      std::cout << "Hi " << serial_num << std::endl;  
     }
     return num;
   }
   ~NestClass() {
-    std::cout << "See you " << serial_num;
-    std::cout <<  global << std::endl;
+    std::cout << "See you " << serial_num << std::endl;;
   }
 };
 
 int main() {
 
   NestClass nest(3);
-    std::cout << nest.sum() << std::endl;
+  std::cout << nest.sum() << std::endl;
 
   return 0;
 }
