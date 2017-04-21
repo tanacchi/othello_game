51659fc4452a952d291597d8e7d6e958a0e2571d
tanacchi <q111026d@mail.kyutech.jp>
2017-04-21 19:53:09 +0900

    Remove extra comment

--- include/OthelloAI.h
+++ include/OthelloAI.h
@@ -1,19 +1,5 @@
 #include "BoardMaster.h"
 
-/*
-
-1. 上の階層から階層番号とボード情報を受け取る
-2. ドットを置いて、それぞれについて点数をつける
-2. 下の階層へ階層番号とスコアの合計を流す
-3. 上限階層orゲーム終了状態になるまで繰り返す
-
-AIは1階層分だけ調べるようにして
-新たにAIを統括するオブジェクトを作成
-
-ScoreList はComputerPlayerにあつかてもらう？？
-
-*/
-
 class StoneScoreList {
   int hand_x;
   int hand_y;
@@ -35,7 +21,6 @@ class OthelloAI {
   std::vector<StoneScoreList> score_list;
   BoardMaster virtual_board;
   int current_depth;
-
  public:
   OthelloAI(BoardMaster game_board, int max_depth);
   OthelloAI();
