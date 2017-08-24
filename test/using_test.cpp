03bbcc2709b28ac55be66ba6ed525b79c78ff553
tanacchi <q111026d@mail.kyutech.jp>
2017-08-24 20:39:00 +0900

    Test using key-word

--- test/using_test.cpp
+++ test/using_test.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
 
-using point = unsigned short;
+using point    = unsigned short;
+using position = std::pair<point, point>;
 
 template<typename T>
 void show(T src)
 {
   std::cout << src << std::endl;
 }
-  
+
+void show(position src)
+{
+  std::cout << "first = "<< src.first << " : second = " << src.second << std::endl;
+}
+
 auto main() -> int
 {
-
   show(sizeof(point));
-  show(sizeof(unsigned short));
+  show(sizeof(int));
   point i = -1;
-  show(i);
+  show((unsigned int)i);
+
+  position p{65, 66};
+  show(p);
   return 0;
 }
