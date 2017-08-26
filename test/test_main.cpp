2f67be0f141897156d91291a0c7398475cf8d341
tanacchi <q111026d@mail.kyutech.jp>
2017-08-26 19:45:23 +0900

    Test BoardMaster's constructor

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-#include "BoardBase.hpp"
+#include "BoardMaster.hpp"
 
 int main()
 {
   position p {std::make_pair(10, 10)};
-  BoardBase board{};
-
+  BoardMaster board{p};
   // std::size_t width, height, length;
   // board.get_size(width, height, length);
   // std::cout << width << ' ' << height << ' ' << length << std::endl;
