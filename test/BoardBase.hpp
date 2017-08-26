ba0e957cf453cdb5d0ea4b1c5140dc51dae597a6
tanacchi <q111026d@mail.kyutech.jp>
2017-08-26 18:19:27 +0900

    Add BoardBase

---
+++ test/BoardBase.hpp
@@ -0,0 +1,42 @@
+#include <valarray>
+
+#include <functional>
+
+using point = unsigned char;
+using position = std::pair<point, point>;
+class BoardBase {
+
+public:
+  enum class Stone {
+    Space,
+    White,
+    Black,
+    Dot
+  };
+  BoardBase();
+  BoardBase(const BoardBase& src);
+  const BoardBase operator=(const BoardBase src);
+  ~BoardBase() = default;
+private:
+  std::valarray<Stone> board_;
+  position size_;
+};
+
+BoardBase::BoardBase()
+  : board_(4),
+    size_ {std::make_pair(2, 2)}
+{  
+}
+
+BoardBase::BoardBase(const BoardBase& src)
+  : board_{src.board_},
+    size_{src.size_}
+{
+}
+
+const BoardBase::BoardBase operator=(const BoardBase src)
+{
+  board_ = src.board_;
+  size_  = src.size_;
+  return *this;
+}
