d450c5af4083700e03cb0389c9d473fdce874b03
tanacchi <q111026d@mail.kyutech.jp>
2017-08-30 02:29:54 +0900

    [WIP]Make simple othello game for test

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -2,61 +2,81 @@
 #include "GameBoard.hpp"
 #include "../include/BoardMaster.hpp"
 
+enum class Task {
+  Init, Op, Set, Insert, Write, Judge, Switch, Ed
+};
+
+Task run(Task task);
+Task task_init();
+Task task_op();
+Task task_set();
+Task task_insert();
+Task task_write();
+Task task_judge();
+Task task_switch();
+
+position size{std::make_pair(10, 10)};
+GameBoard board{size};
+int turn;
+position current_pos;
+
+Task run(Task task)
+{
+  switch (task) {
+  case Task::Init:   return task_init();
+  case Task::Op:     return task_op();
+  case Task::Set:    return task_set();
+  case Task::Insert: return task_insert();
+  case Task::Write:  return task_write();
+  case Task::Judge:  return task_judge();
+  case Task::Switch: return task_switch();
+  case Task::Ed:
+  default:           throw std::invalid_argument{"It's wrong task!!"};
+  }
+}
+
+Task task_init()
+{
+  turn = 0;
+  board.init();
+  return Task::Op;
+}
+
+Task task_op()
+{
+  
+}
+
+Task task_set()
+{
+
+}
+
+Task task_insert()
+{
+
+}
+
+Task task_write()
+{
+
+}
+  
+Task task_judge()
+{
+
+}
+
+Task task_switch()
+{
+
+}
+
 int main()
 {
-  position p {std::make_pair(10, 10)};
-  GameBoard board{p};
-
   std::cout << "sizeof(BoardBase) = " << sizeof(BoardBase) << ", "
             << "sizeof(GameBoard) = " << sizeof(GameBoard) << std::endl;
   std::cout << "sizeof(BoardMaster) = " << sizeof(BoardMaster) << std::endl;
-  
-  // std::size_t width, height, length;
-  // board.get_size(width, height, length);
-  // std::cout << width << ' ' << height << ' ' << length << std::endl;
-
-  // std::valarray<int> a(12);
-  // std::iota(std::begin(a), std::end(a), 0);
-  // for (auto x : a) std::cout << x << ' ' << std::flush;
-  // std::cout.put('\n');
-  
-  // std::valarray<int> b(13);
-  // std::iota(std::begin(b), std::end(b), 100);
-  // for (auto x : b) std::cout << x << ' ' << std::flush;
-  // std::cout.put('\n');
-
-  // a = b;
-  // for (auto x : a) std::cout << x << ' ' << std::flush;
-  // std::cout.put('\n');
-
-  BoardBase board1{};
-  BoardBase board2{board1};
-  BoardBase board3{};  
-
-  // board3 = board1;
-
-  // char c = board.to_char((BoardBase::Stone)5);
-  // std::cout << c << std::endl;
-  board.init();
-  board.show();
-
-  // for (int i{0}; i < 10; std::cout.put('\n'), i++)
-  //   for (int j{0}; j < 10; std::cout.put(' '), j++)
-  //     for (int k{0}; k < 8; k++)
-  //       std::cout << board.get_reversible_length(std::make_pair(j,i), direction[k]);
-
-   for (int i{0}; i < 10; i++)
-     for (int j{0}; j < 10; j++)
-       if (board.can_reverse(std::make_pair(j, i)))
-         std::cout << "x = " << j+1 << ", y = " << i+1 << std::endl;;
-
-  std::cout << "Space : " << board.count_stone(BoardBase::Stone::Space) << '\n'
-            << "White : " << board.count_stone(BoardBase::Stone::White) << '\n'
-            << "Black : " << board.count_stone(BoardBase::Stone::Black) << std::endl;
-
-  position pos{std::make_pair(6, 4)};
-  board.insert(pos);
-  board.reverse(pos);
   board.show();
   return 0;
 }
