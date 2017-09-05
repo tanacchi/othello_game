549682e91f0995524f180cb244b5ff8e281a9cb4
tanacchi <q111026d@mail.kyutech.jp>
2017-09-06 02:17:40 +0900

    Update get_status_score method

--- test/AiBoard.hpp
+++ test/AiBoard.hpp
@@ -5,14 +5,24 @@
 
 class AiBoard : public BoardBase {
 public:
-  AiBoard();
+  AiBoard(const BoardBase& src);
   const AiBoard& operator=(const AiBoard& src) = delete;
   double get_status_score() const;
 };
 
+AiBoard::AiBoard(const BoardBase& src)
+  : BoardBase(src)
+{
+}
+
 double AiBoard::get_status_score() const
 {
   double status_score {0};
+  std::valarray<std::size_t> edge_pos = {
+    0,                             get_access_num(width()-1, 0),
+    get_access_num(0, height()-1), width()*height()-1
+  };
+  std::valarray<BoardBase::Stone> edge{board_[edge_pos]};
   return status_score;
 }
 
