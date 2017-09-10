2fd2973cb93e30bdd31f72ed6ecc0b5877b84bcb
tanacchi <q111026d@mail.kyutech.jp>
2017-09-11 02:30:49 +0900

    Test OthelloAI
    (動作)おっっっせえなオイ！！！

--- src/PlayerSeries.cpp
+++ src/PlayerSeries.cpp
@@ -61,7 +61,7 @@ ComputerPlayer::ComputerPlayer()
 
 bool ComputerPlayer::set_hand(const BoardSeries::GameBoard& game_board)
 {
-  OthelloAI seeker{game_board, 5};
+  OthelloAI seeker{game_board, 4};
   hand_ = seeker.get_conclusion();
   return true;
 }
