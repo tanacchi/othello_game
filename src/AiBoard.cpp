efa1294c9a2a7482f9af8594ed383922c53218e0
tanacchi <q111026d@mail.kyutech.jp>
2017-09-14 23:16:28 +0900

    Move array for evaluate

--- src/AiBoard.cpp
+++ src/AiBoard.cpp
@@ -12,36 +12,18 @@ float AiBoard::get_status_score() const
   const Stone enemy_stone {get_enemy_stone()};
   float status_score {static_cast<float>(count_stone(active_stone_) - count_stone(enemy_stone))};
   Stone target{Stone::Space};
-  static const std::vector<Position> edge_pos = {
-    {0, 0}, {7, 0},
-    {0, 7}, {7, 7}
-  };
-  for (int i{0}; i < 4; ++i) {
-    target = board_[get_access_num(edge_pos[i])];
+  for (const auto& edge : edge_pos) {
+    target = board_[get_access_num(edge)];
     if (target == active_stone_) status_score += 15;
     else if (target == enemy_stone) status_score += -15;
   }
-  static const std::vector<Position> near_pos = {
-            {1, 0}, {6, 0},
-    {0, 1}, {1, 1}, {6, 1}, {7, 1},
-    {0, 6}, {1, 6}, {6, 6}, {7, 6},
-            {1, 7}, {6, 7}
-  };
-  for (int i{0}; i < 12; ++i) {
-    target = board_[get_access_num(near_pos[i])];
+  for (const auto& near : near_pos) {
+    target = board_[get_access_num(near)];
     if (target == active_stone_) status_score += -5;
     else if (target == enemy_stone) status_score += 5;
   }
-  static const std::vector<Position> reach_pos = {
-                    {2, 0}, {5, 0},
-                    {2, 1}, {5, 1},
-    {0, 2}, {1, 2}, {2, 2}, {5, 2}, {6, 2}, {7, 2},
-    {0, 5}, {1, 5}, {2, 5}, {5, 5}, {6, 5}, {7, 5},
-                    {2, 6}, {5, 6},
-                    {2, 7}, {5, 7}
-  };
-  for (int i{0}; i < 8; ++i) {
-    target = board_[get_access_num(reach_pos[i])];
+  for (const auto& reach : reach_pos) {
+    target = board_[get_access_num(reach)];
     if (target == active_stone_) status_score += 5;
     else if (target == enemy_stone) status_score += -5;
   }
