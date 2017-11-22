75c5a82afb7f9405e48c769f9f05931d4057bb51
tanacchi <q111026d@mail.kyutech.jp>
2017-11-22 10:39:31 +0900

    Use faster type for Vector2D::Point

--- include/Vector2D.hpp
+++ include/Vector2D.hpp
@@ -1,9 +1,11 @@
 #ifndef VECTOR_2D_H_
 #define VECTOR_2D_H_
 
+#include <cstdint>
+
 struct Vector2D {
 public:
-  using Point = char;
+  using Point = std::int_fast8_t;
   Point x, y;
   Vector2D(Point init_x, Point init_y);
   Vector2D operator+(const Vector2D& src) const;
